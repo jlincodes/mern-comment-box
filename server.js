00bbbd8aa678ed75f67b2189f3e1907b31e65dd4
Julie <julie.lin0812@gmail.com>
2018-03-14 15:41:26 -0700

    Adds GET, POST, UPDATE, and DELETE routes

--- server.js
+++ server.js
@@ -1,52 +1,210 @@
+// 'use strict';
+//
+// // import dependencies
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bodyParser = require('body-parser');
+// const Comment = require('./model/comments');
+//
+// // create instances
+// const app = express();
+// const router = express.Router();
+//
+// // set port to either a predetermined port number or 3001
+// const port = process.env.API_PORT || 3001;
+//
+// // db config
+// const mongoDB = 'mongodb://<test-user2>:<qE92gD>@ds213229.mlab.com:13229/mern-comment-box';
+// mongoose.connect(mongoDB);
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+//
+// // congiures the API to use bodyParser and look for JSON data in req body
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+//
+// // To prevent errors from Cross Origin Resource Sharing,
+// // set headers to allow CORS with middleware
+// app.use(function(req, res, next) {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   res.setHeader('Access-Control-Allow-Methods',
+//                 'GET, HEAD, OPTIONS, POST, PUT, DELETE');
+//   res.setHeader('Access-Control-Allow-Headers',
+//                 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+//
+//   // remove cacheing to get most recent comments
+//   res.setHeader('Cache-Control', 'no-cache');
+//   next();
+// });
+//
+// // set the route path and initialize the API
+// router.get('/', function(req, res) {
+//   res.json({ message: 'API Initialized!' });
+// });
+//
+// // adds /comments route to /api router
+// router.route('/comments')
+//   // retrieve all comments from the DB
+//   .get(function(req, res) {
+//     Comment.find(function(err, comments) {
+//       if (err) res.send(err);
+//       res.json(comments);
+//     });
+//   })
+//   // post new comment to the DB
+//   .post(function(req, res) {
+//     const comment = new Comment();
+//     // body parser lets us use the req.body
+//     comment.author = req.body.author;
+//     comment.text = req.body.text;
+//
+//     comment.save(function(err) {
+//       if (err) res.send(err);
+//       res.json({ message: 'Comment successfully added!' });
+//     });
+//   });
+//
+// //Adding a route to a specific comment based on the database ID
+// router.route('/comments/:comment_id')
+//   //The put method gives us the chance to update our comment based on the ID passed to the route
+//  .put(function(req, res) {
+//    Comment.findById(req.params.comment_id, function(err, comment) {
+//      if (err)
+//        res.send(err);
+//      //setting the new author and text to whatever was changed.
+//      //If nothing was changed we will not alter the field.
+//      (req.body.author) ? comment.author = req.body.author : null;
+//      (req.body.text) ? comment.text = req.body.text : null;
+//      //save comment
+//      comment.save(function(error) {
+//        if (error)
+//          res.send(error);
+//        res.json({ message: 'Comment has been updated' });
+//      });
+//    });
+//  })
+//  //delete method for removing a comment from our database
+//  .delete(function(req, res) {
+//    //selects the comment by its ID, then removes it.
+//    Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+//      if (err)
+//        res.send(err);
+//      res.json({ message: 'Comment has been deleted' });
+//    });
+//  });
+//
+// // use router configuration when API is called
+// app.use('/api', router);
+//
+// // starts server and listens for requests
+// app.listen(port, function() {
+//   console.log(`api running on port ${port}`);
+// });
+
+
+//server.js
 'use strict';
 
-// import dependencies
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const Comment = require('./model/comments');
+//first we import our dependencies...
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Comment = require('./model/comments');
+var secrets = require('./secrets');
 
-// create instances
-const app = express();
-const router = express.Router();
+//and create our instances
+var app = express();
+var router = express.Router();
 
-// set port to either a predetermined port number or 3001
-const port = process.env.API_PORT || 3001;
+//set our port to either a predetermined port number if you have set it up, or 3001
+var port = process.env.API_PORT || 3001;
 
-// db config
-const mongoDB = 'mongodb://test-user:o&Bbg1i6$JW8G@ds213229.mlab.com:13229/mern-comment-box';
-mongoose.connect(mongoDB, { useMongoClient: true });
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error'));
+//db config -- set your URI from mLab in secrets.js
+var mongoDB = secrets.requestSecret('db_uri');
+mongoose.connect(mongoDB, {});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
-// congiures the API to use bodyParser and look for JSON data in req body
+//now we should configure the APi to use bodyParser and look for JSON data in the body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// To prevent errors from Cross Origin Resource Sharing,
-// set headers to allow CORS with middleware
+//To prevent errors from Cross Origin Resource Sharing, we will set our headers to allow CORS with middleware like so:
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Methods',
-                'GET, HEAD, OPTIONS, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers',
-                'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
 
-  // remove cacheing to get most recent comments
+  //and remove cacheing so we get the most recent comments
   res.setHeader('Cache-Control', 'no-cache');
   next();
 });
 
-// set the route path and initialize the API
+//now  we can set the route path & initialize the API
 router.get('/', function(req, res) {
-  res.json({ message: 'API Initialized!' });
+  res.json({ message: 'API Initialized!'});
 });
 
-// use router configuration when API is called
+//adding the /comments route to our /api router
+router.route('/comments')
+  //retrieve all comments from the database
+  .get(function(req, res) {
+    //looks at our Comment Schema
+    Comment.find(function(err, comments) {
+      if (err)
+        res.send(err);
+      //responds with a json object of our database comments.
+      res.json(comments);
+    });
+  })
+  //post new comment to the database
+  .post(function(req, res) {
+    var comment = new Comment();
+    (req.body.author) ? comment.author = req.body.author : null;
+    (req.body.text) ? comment.text = req.body.text : null;
+
+    comment.save(function(err) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment successfully added!' });
+    });
+  });
+
+//Adding a route to a specific comment based on the database ID
+router.route('/comments/:comment_id')
+//The put method gives us the chance to update our comment based on the ID passed to the route
+  .put(function(req, res) {
+    Comment.findById(req.params.comment_id, function(err, comment) {
+      if (err)
+        res.send(err);
+      //setting the new author and text to whatever was changed. If nothing was changed
+      // we will not alter the field.
+      (req.body.author) ? comment.author = req.body.author : null;
+      (req.body.text) ? comment.text = req.body.text : null;
+      //save comment
+      comment.save(function(err) {
+        if (err)
+          res.send(err);
+        res.json({ message: 'Comment has been updated' });
+      });
+    });
+  })
+  //delete method for removing a comment from our database
+  .delete(function(req, res) {
+    //selects the comment by its ID, then removes it.
+    Comment.remove({ _id: req.params.comment_id }, function(err, comment) {
+      if (err)
+        res.send(err);
+      res.json({ message: 'Comment has been deleted' });
+    });
+  });
+
+//Use our router configuration when we call /api
 app.use('/api', router);
 
-// starts server and listens for requests
-app.listen(port, function() {
-  console.log(`api running on port ${port}`);
-});
+//starts the server and listens for requests
+// app.listen(port, function() {
+//   console.log(`api running on port ${port}`);
+// });
--- src/CommentBox.js
+++ src/CommentBox.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
-import DATA from '../data';
 import style from './style';
 
 class CommentBox extends Component {
@@ -9,12 +9,43 @@ class CommentBox extends Component {
     super(props);
     this.state = { data: [] };
   }
+  loadCommentsFromServer() {
+    if (this.props) {
+      axios.get(this.props.url)
+        .then(res => {
+          this.setState({ data: res.data });
+        });
+    }
+  }
+
+  handleCommentSubmit(comment) {
+    if (this.props) {
+      let comments = this.state.data;
+      comment._id = Date.now();
+      let newComments = comments.concat([comment]);
+      this.setState({ data: newComments });
+      axios.post(this.props.url, comment)
+        // .then(res => {
+        //   this.loadCommentsFromServer();
+        // })
+        .catch(err => {
+          console.error(err);
+          this.setState({ data: comments });
+      });
+    }
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
+  }
+
   render() {
     return (
       <div style={ style.commentBox }>
         <h2>Comments:</h2>
-      <CommentList data={ DATA }/>
-      <CommentForm />
+        <CommentList data={ this.state.data }/>
+        <CommentForm onCommentSubmit={ this.handleCommentSubmit }/>
       </div>
     );
   }
