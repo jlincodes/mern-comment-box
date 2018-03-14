d5965c97f9cc634738fa5c2f2d12fe81ce82eefb
Julie <julie.lin0812@gmail.com>
2018-03-14 13:19:08 -0700

    Adds rest of file structure for skeleton

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import CommentBox from './CommentBox';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('root')
+);
